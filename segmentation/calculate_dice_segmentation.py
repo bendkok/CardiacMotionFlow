7496e6aa996482aa7bff64b30dd80c2778d3112d
bendkok <31344710+bendkok@users.noreply.github.com>
2022-10-31 14:19:53 +0100

    Update calculate_dice_segmentation.py

--- segmentation/calculate_dice_segmentation.py
+++ segmentation/calculate_dice_segmentation.py
@@ -45,16 +45,22 @@ def calculate_dice_segmentation(dataset = 'acdc'):
     
     
     if dataset == 'acdc':
-        seq_segs_pre = []
+        
+        seq_segs_pre_train = []
         for f in range(1,6):
             seq_context_imgs, seq_context_segs, seq_imgs, seq_segs = data_lvrv_segmentation_propagation_acdc(mode = 'val_predict', fold = f)
-            seq_segs_pre += [seq_segs[se] + seq_segs[se+1] for se in range(len(seq_segs))[::2]]
-            # seq_segs_pre += seq_segs
-        # seq_context_imgs, seq_context_segs, seq_imgs, seq_segs_pre = data_lvrv_segmentation_propagation_acdc(mode = 'predict', fold = fold)
-        # seq_segs_pre = seq_segs_pre[::2]
-        seq_segs_pre = np.sort(seq_segs_pre)
-        seq_segs_gt = [[a.replace('predict_lvrv2_', 'crop_2D_gt_', 1) for a in b ] for b in seq_segs_pre]
-        seq_segs_gt = [[a.replace('predict_2D', 'crop_2D', 1) for a in b ] for b in seq_segs_gt]
+            seq_segs_pre_train += [seq_segs[se] + seq_segs[se+1] for se in range(len(seq_segs))[::2]]
+        
+        # seq_context_imgs, seq_context_segs, seq_imgs, seq_segs = data_lvrv_segmentation_propagation_acdc(mode = 'predict')
+        # seq_segs_pre_test = [seq_segs[se] + seq_segs[se+1] for se in range(len(seq_segs))[::2]]
+        
+        seq_segs_pre_train = np.sort(seq_segs_pre_train)
+        # seq_segs_pre_test = np.sort(seq_segs_pre_test)
+        
+        seq_segs_gt_train = [[a.replace('predict_lvrv2_', 'crop_2D_gt_', 1) for a in b ] for b in seq_segs_pre_train]
+        seq_segs_gt_train = [[a.replace('predict_2D', 'crop_2D', 1) for a in b ] for b in seq_segs_gt_train]
+        # seq_segs_gt_test = [[a.replace('predict_lvrv2_', 'crop_2D_gt_', 1) for a in b ] for b in seq_segs_pre_test]
+        # seq_segs_gt_test = [[a.replace('predict_2D', 'crop_2D', 1) for a in b ] for b in seq_segs_gt_test]
         #hopefully this works
     elif dataset == 'mesa':
         seq_context_imgs, seq_context_segs, seq_imgs, seq_segs = data_mesa_lvrv_segmentation_propagation_acdc(mode = mode, fold = fold)
@@ -66,51 +72,56 @@ def calculate_dice_segmentation(dataset = 'acdc'):
         
     # seq_segs = the ground thruths
     
-    predict_sequence = len(seq_segs_pre)
+    predict_sequence_train = len(seq_segs_pre_train)
+    # predict_sequence_test = len(seq_segs_pre_test)
     
-    dice_scores = []
-    dice_scores2 = []
-    # num_processed = 0
-    print(predict_sequence)
-    for i in range(predict_sequence):
-        
-        segs_pre = seq_segs_pre[i]
+    dice_scores_train = []
+    dice_scores_test = []
+    # print(f'Number of train subjects: {predict_sequence_train}. Number of test subjects: {predict_sequence_test}.')
+    print(f'\nNumber of train subjects: {predict_sequence_train}.')
+    for i in range(predict_sequence_train):    
+        segs_pre = seq_segs_pre_train[i]
         #the segs_gt is not grouped 
-        segs_gt = seq_segs_gt[i]
-        # num_processed += len(segs_pre)
+        segs_gt = seq_segs_gt_train[i]
         
         if len(segs_pre) != len(segs_gt):
             print(f"Not same length! {i}")
         
         segs_pre_im = np.array([iio.imread(se) for se in segs_pre], dtype=float)
         segs_gt_im = np.array([iio.imread(se) for se in segs_gt], dtype=float)
-        
-        # segs_pre_im = iio.imread(segs_pre)
-        # segs_gt_im = iio.imread(segs_gt)
-        
+
         curr_dice2 = dice_coef2(segs_gt_im, segs_pre_im).numpy()
-    
-        segs_pre_im = tf.where(segs_pre_im > 0.5, K.ones_like(segs_pre_im), K.zeros_like(segs_pre_im))
-        segs_gt_im = tf.where(segs_gt_im > 0.5, K.ones_like(segs_gt_im), K.zeros_like(segs_gt_im))
-        curr_dice = dice_coef(segs_gt_im, segs_pre_im).numpy()
+        dice_scores_train.append(curr_dice2)
         
-        dice_scores.append(curr_dice)
-        dice_scores2.append(curr_dice2)
+    # for i in range(predict_sequence_test):    
+    #     segs_pre = seq_segs_pre_test[i]
+    #     #the segs_gt is not grouped 
+    #     segs_gt = seq_segs_gt_test[i]
         
-    return dice_scores,dice_scores2
+    #     if len(segs_pre) != len(segs_gt):
+    #         print(f"Not same length! {i}")
+        
+    #     segs_pre_im = np.array([iio.imread(se) for se in segs_pre], dtype=float)
+    #     segs_gt_im = np.array([iio.imread(se) for se in segs_gt], dtype=float)
+
+    #     curr_dice2 = dice_coef2(segs_gt_im, segs_pre_im).numpy()
+    #     dice_scores_test.append(curr_dice2)
+        
+    return [dice_scores_train]#, dice_scores_test
 
 
 if __name__ == '__main__':
     #I think dice_coef2 is the correct one
     dice = calculate_dice_segmentation()
+    print(dice[0])    
+    df = pd.DataFrame(dice[0])
     print()
-    print(dice[0])
-    print()
-    print(dice[1])
-    print(max(dice[1]), min(dice[1]))
-    
-    df = pd.DataFrame(dice[1])
     print(df.describe())
+    
+    # print()
+    # print(dice[1])    
+    # df = pd.DataFrame(dice[1])
+    # print(df.describe())
     # print(np.where(np.array(dice[0]) - np.array(dice[1]) < 1e-1)[0], len(np.where(np.array(dice[0]) - np.array(dice[1]) < 1e-1)[0]))
     # print(np.where(np.array(dice[0]) - np.array(dice[1]) < 1e-2)[0], len(np.where(np.array(dice[0]) - np.array(dice[1]) < 1e-2)[0]))
     # print(np.where(np.array(dice[0]) - np.array(dice[1]) < 1e-3)[0], len(np.where(np.array(dice[0]) - np.array(dice[1]) < 1e-3)[0]))
